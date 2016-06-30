f2816b32b568e9e3223e52e6a20a2cb669762bbd
Federico Rota <federico.rota01@gmail.com>
2016-06-30 16:36:35 +0200

    Removed unused deps and set correct urls

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,21 +12,6 @@ module.exports = function(grunt) {
 
     // Project configuration.
     grunt.initConfig({
-        jshint: {
-            all: [
-                'Gruntfile.js',
-                'tasks/*.js',
-                '<%= nodeunit.tests %>'
-            ],
-            options: {
-                jshintrc: '.jshintrc'
-            }
-        },
-
-        // Before generating any new files, remove any previously-created files.
-        clean: {
-            tests: ['tmp']
-        },
 
         // Configuration to be run (and then tested).
         json_checker_gruntplugin: {
@@ -46,12 +31,6 @@ module.exports = function(grunt) {
 
     // Actually load this plugin's task(s).
     grunt.loadTasks('tasks');
-
-    // These plugins provide necessary tasks.
-    grunt.loadNpmTasks('grunt-contrib-jshint');
-    grunt.loadNpmTasks('grunt-contrib-clean');
-    grunt.loadNpmTasks('grunt-contrib-nodeunit');
-
 
     // By default, lint and run all tests.
     grunt.registerTask('default', ['json_checker_gruntplugin']);
